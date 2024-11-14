0fcea2ee995a72f0d9a796e2888263175ea0f046
christinehatch <christinehatch12@mittymonarch.com>
2024-11-14 13:42:14 -0800

    Create AddAccountView.java

---
+++ PasswordManager/utils/AddAccountView.java
@@ -0,0 +1,21 @@
+package utils;
+
+import javax.swing.*;
+
+//create new account page
+public class  AddAccountView extends JFrame {
+
+    private JPanel contentPane;
+    private JLabel WebsiteLabel;
+    private JLabel UsernameLabel;
+    private JLabel PasswordLabel;
+    private JTextField WebsiteTextField;
+    private JTextField UsernameTextField;
+    private JTextField PasswordTextField;
+    private JButton addAccountButton;
+    private JButton cancelButton;
+    private JRadioButton accountTypeRadioButton;
+    private JRadioButton passwordTypeRadioButton;
+
+
+}
